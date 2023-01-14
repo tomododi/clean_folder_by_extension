38d6199c815785715517b3550bba98b602b9b56d
tomododi <tomdudek@hotmail.com>
2023-01-14 01:29:13 +0100

    a few adjustments to the colors and positioning

--- main.py
+++ main.py
@@ -7,26 +7,28 @@ from tkinter import *
 
 
 root = Tk()
-root.title("Folder Cleaner")
-root.geometry("300x150")
 
+root.title("Folder Cleaner")
+root.config(bg='#add8e6')
+root.geometry("350x200")
+root.resizable(width=False, height=False)
 folder_path_var = StringVar()
 folder_path_entry = Entry(root, textvariable=folder_path_var, width=30)
-folder_path_entry.pack()
+folder_path_entry.pack(side=TOP)
 
 def select_folder():
     folder_selected = filedialog.askdirectory()
     folder_path_var.set(folder_selected)
 
-select_folder_button = Button(root, text = "Select Folder", command = select_folder)
+select_folder_button = Button(root, text = "Select Folder", activebackground='#e6b2ad', bg='#e6bbad', command = select_folder)
 select_folder_button.pack()
 
 def on_clean():
     folder_path = folder_path_var.get()
     organize_folder(folder_path)
     
-clean_button = Button(root, text = "Clean Folder", command = on_clean)
-clean_button.pack()
+clean_button = Button(root, text = "Clean Folder",activebackground='#e6b2ad', bg='#e6bbad', command = on_clean)
+clean_button.pack(side=BOTTOM)
 
 
 
